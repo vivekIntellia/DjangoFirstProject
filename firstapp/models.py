5f615022c86971e7f864d070537cafd7cd67f774
Ayushi jain <aayushi.jain@intelliatech.com>
2024-03-07 18:36:44 +0530

    email varification done

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
 
 class firstapp(models.Model):
     firstapp_icon = models.CharField(max_length=50)
     firstapp_title = models.CharField(max_length=50)
     firstapp_des = models.TextField()
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_verified = models.BooleanField(default=False)
+    verification_token = models.CharField(max_length=100, blank=True, null=True)
+    cerated_at=models.DateField(auto_now_add=True)
 
 
-# Create your models here.
+    def __str__(self):
+        return self.user.username
 
-# Create your models here.
+
+
+
--- newproject/urls.py
+++ newproject/urls.py
@@ -23,16 +23,24 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.SignupPage, name='signup'),
-    path('signup/', views.SignupPage, name='signup'),
-    path('login/', views.LoginPage , name='login'),
-    path('home/', views.HomePage , name='home'),
-    path('logout/',views.LogoutPage,name='logout'),
-    path('services/', views.services , name='services'),
-    path('generate-pdf/<int:service_id>/', views.generate_pdf, name='generate_pdf'),
-    path('generate-excel/<int:service_id>/', views.generate_excel, name='generate_excel'),
-    path('download-csv/<int:service_id>/', views.download_csv, name='download_csv'),
-    path('form', views.form , name='form'),
-    path('thankyou/', views.thankyou , name='thankyou'),
+    path('',include('firstapp.urls'))
+    # # path('signup/', views.SignupPage, name='signup'),
+    # path('login/', views.LoginPage , name='login'),
+    # path('home/', views.HomePage , name='home'),
+    # path('logout/',views.LogoutPage,name='logout'),
+    # path('services/', views.services , name='services'),
+    # path('generate-pdf/<int:service_id>/', views.generate_pdf, name='generate_pdf'),
+    # path('generate-excel/<int:service_id>/', views.generate_excel, name='generate_excel'),
+    # path('download-csv/<int:service_id>/', views.download_csv, name='download_csv'),
+    # path('form', views.form , name='form'),
+    # path('thankyou/', views.thankyou , name='thankyou'),
+    # path('token_send/',views.token_send,name='token_send'),
+    # path('success/',views.success,name='success'),
+    # path('verify/<verification_token>',views.verify,name='verify'),
+    # path('error/',views.error_page,name='error'),
+
+
+
     # path('upload/', views.upload_image, name='upload_image'),
     # path('calculator/', views.calculator),
 
