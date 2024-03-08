bfb0e37f7271c3feed5d7352b7a2cdf251f86ddc
Vivek <vivek.yadav@intelliatech.com>
2024-03-08 11:37:13 +0530

    Commit for code change

--- firstapp/models.py
+++ firstapp/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 
-
-class firstapp(models.Model):
-    firstapp_icon = models.CharField(max_length=50)
-    firstapp_title = models.CharField(max_length=50)
-    firstapp_des = models.TextField()
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email_verified = models.BooleanField(default=False)
--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Services , Profile
+from .models import Services
 
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('service_icon', 'service_title', 'service_des')
@@ -8,4 +8,4 @@ class ServiceAdmin(admin.ModelAdmin):
 admin.site.register(Services, ServiceAdmin)
 
 # Register CustomUser with UserAdmin
-admin.site.register(Profile)
+# admin.site.register(Profile)
