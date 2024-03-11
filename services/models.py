3513ae34d917f57f92a2a04583dd517add4334fa
Vivek <vivek.yadav@intelliatech.com>
2024-03-11 12:24:07 +0530

    Commit for user detail and admin approval page

--- services/models.py
+++ services/models.py
@@ -6,9 +6,7 @@ class Services(models.Model):
     service_title = models.CharField(max_length=50)
     service_des=models.TextField()
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User , on_delete = models.CASCADE , related_name = 'Profile')
-#     image = models.ImageField(upload_to = 'pics')
+
 
 
 
